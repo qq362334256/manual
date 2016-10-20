2900d8e067e8df3c4f7d6956b828f932b18c15cc
miaoyu <362334256@qq.com>
2016-10-20 18:43:47 +0800

    2016-10-20 (服务路由更新) 喵鱼 ----------------------------------- 新增：         1、'/' 路由         2、'/index' 路由

--- router.js
+++ router.js
@@ -9,6 +9,9 @@ let viewTem = require('./views/core/core.js');
 // 挂在模块
 exports.setRouter = function(app) {
     // 首页通用路由
-    app.get('*', (req, res) => res.send(viewTem.getTemplate('/index')));
-    //app.get('/index', (req, res) => res.send(viewTem.getTemplate(req.url)));
-}
+    app.get('/index', (req, res) => res.send(viewTem.getTemplate(req.url)));
+    //app.get('*', (req, res) => res.send(viewTem.getTemplate('/index')));
+
+    // 跳首页
+    app.get('/', (req, res) => res.send(viewTem.getTemplate(req.url)));
+};
